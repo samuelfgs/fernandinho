3f3eae92d92b1f98e50f575325d8e67e1a682ebb
samuelfgs <samuel.fg96@gmail.com>
2024-08-14 03:47:00 -0300

    add meta tag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,4 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
 import { PlasmicRootProvider } from "@plasmicapp/react-web";
 import type { AppProps } from "next/app";
 import Head from "next/head";
@@ -6,6 +6,12 @@ import Head from "next/head";
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <PlasmicRootProvider Head={Head}>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1"
+        />
+      </Head>
       <Component {...pageProps} />
     </PlasmicRootProvider>
   );
