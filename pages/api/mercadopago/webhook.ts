07ba6a3e9357e43ec54ace1546a12bf640ff9562
samuelfgs <samuel.fg96@gmail.com>
2024-08-14 02:47:54 -0300

    fix webhook

--- pages/api/mercadopago/webhook.ts
+++ pages/api/mercadopago/webhook.ts
@@ -13,7 +13,7 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const id = req.query?.["data.id"];
+  const paymentId = req.query?.["data.id"];
   const topic = req.query?.type;
 
   if (topic !== "payment") {
@@ -22,26 +22,27 @@ export default async function handler(
   }
 
   const mercadoPago = await (
-    await fetch(`https://api.mercadopago.com/v1/payments/${id}`, {
+    await fetch(`https://api.mercadopago.com/v1/payments/${paymentId}`, {
       headers: {
         authorization: `Bearer ${process.env.ACCESS_TOKEN}`,
       },
     })
   ).json();
 
-  console.log("dale1", { id, mercadoPago });
+  console.log("dale1", { paymentId, mercadoPago });
   if (mercadoPago.status !== "approved") {
     console.log("dale", "not paid");
     res.status(500).json("not paid");
     return;
   }
 
+  const mercadoPagoId = mercadoPago.external_reference;
   const { data: inscritoData, error: inscritoError } = await supabase
     .from('inscritos_fernandinho')
     .select("*")
-    .eq("mercadopago", id);
+    .eq("mercadoPagoId", mercadoPagoId);
 
-  console.log("dale2", { id, inscritoData })
+  console.log("dale2", { mercadoPagoId, inscritoData })
   if (inscritoError || inscritoData.length !== 1) {
     throw new Error(inscritoError?.message ?? "Unknown error");
   }
@@ -58,10 +59,10 @@ export default async function handler(
       method: mercadoPago.payment_method_id,
       lote: inscrito.lote,
      })
-    .eq('id', +mercadoPago.external_reference)
+    .eq('user_id', inscrito.id )
     .select("*");
 
-  console.log("dale3", { id, paymentData });
+  console.log("dale3", { id: inscrito.id, paymentData });
   if (paymentError || !paymentData || paymentData.length !== 1) {
     console.log("paymentError", { paymentError, paymentData })
     throw new Error(paymentError?.message ?? "Unknown error");
@@ -90,7 +91,7 @@ export default async function handler(
   }
 
   const { error: error2 } = await supabase
-    .from('inscritos')
+    .from('inscritos_fernandinho')
     .update({ sent_email: true })
     .eq('id', inscrito.id)
 
